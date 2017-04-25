cf36af163ac2d43ed3f62c2e5e60103f1842f429
Matthew Bennett <mbennett@practicefusion.com>
2017-04-24 17:21:15 -0700

    Adds undo to dummy app

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    color: '#0000ff',
+    height: 68,
+    weight: 1,
+    width: 386,
     signature: Ember.computed(function () {
         return Ember.A();
     }),
@@ -8,8 +12,17 @@ export default Ember.Controller.extend({
         return JSON.stringify(this.get('signature'));
     }),
     actions: {
-        reset: function() {
+        reset() {
             this.set('signature', Ember.A());
+        },
+        undo() {
+            let lastNewLine;
+            this.get('signature').forEach((item, index) => {
+                if (item[0] === 1) {
+                    lastNewLine = index;
+                }
+            });
+            this.set('signature', Ember.A(this.get('signature').slice(0, lastNewLine)));
         }
     }
 });
